419c9e1665352e2a99e39f777ad64b79fb5130c5
leehanjeong <rosy0111@naver.com>
2020-10-16 03:30:48 +0900

    second commit: Cart화면 CheckBox invisible 설정 전까지

--- app/src/main/java/com/example/project_20191650/CartActivity.java
+++ app/src/main/java/com/example/project_20191650/CartActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -19,8 +20,34 @@ public class CartActivity extends Activity {
         Button btn_cart_buy = (Button) findViewById(R.id.btn_cart_buy);
         Button btn_home = (Button) findViewById(R.id.btn_home);
 
+        TextView text1 = (TextView) findViewById(R.id.text1);
+        TextView text2 = (TextView) findViewById(R.id.text2);
+        TextView text3 = (TextView) findViewById(R.id.text3);
+        TextView text4 = (TextView) findViewById(R.id.text4);
+        TextView text5 = (TextView) findViewById(R.id.text5);
+        TextView text6 = (TextView) findViewById(R.id.text6);
+        TextView text7 = (TextView) findViewById(R.id.text7);
+        TextView text8 = (TextView) findViewById(R.id.text8);
+
+        TextView text1_1 = (TextView) findViewById(R.id.text1_1);
+        TextView text2_1 = (TextView) findViewById(R.id.text2_1);
+        TextView text3_1 = (TextView) findViewById(R.id.text3_1);
+        TextView text4_1 = (TextView) findViewById(R.id.text4_1);
+        TextView text5_1 = (TextView) findViewById(R.id.text5_1);
+        TextView text6_1 = (TextView) findViewById(R.id.text6_1);
+        TextView text7_1 = (TextView) findViewById(R.id.text7_1);
+        TextView text8_1 = (TextView) findViewById(R.id.text8_1);
+
+        TextView[] textList = {text1, text2, text3, text4, text5, text6, text7, text8};
+        TextView[] text_List = {text1_1, text2_1, text3_1, text4_1, text5_1, text6_1, text7_1, text8_1};
+
         ArrayList<String> checkedItem = (ArrayList<String>) intent.getSerializableExtra("checkedItem");
         ArrayList<String> checkedPrice = (ArrayList<String>) intent.getSerializableExtra("checkedPrice");
+
+        for(int i=0; i<checkedItem.size(); i++) {
+            textList[i].setText(checkedItem.get(i));
+            text_List[i].setText(checkedPrice.get(i));
+        }
 
         btn_cart_buy.setOnClickListener(new View.OnClickListener(){
             @Override
--- app/src/main/java/com/example/project_20191650/SelectActivity.java
+++ app/src/main/java/com/example/project_20191650/SelectActivity.java
@@ -13,72 +13,12 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class SelectActivity extends AppCompatActivity {
-    //view 선언
-    Button btn_cart;
-    Button btn_buy;
-    CheckBox cb1;
-    CheckBox cb2;
-    CheckBox cb3;
-    CheckBox cb4;
-    CheckBox cb5;
-    CheckBox cb6;
-    CheckBox cb7;
-    CheckBox cb8;
-
-    TextView tv1;
-    TextView tv2;
-    TextView tv3;
-    TextView tv4;
-    TextView tv5;
-    TextView tv6;
-    TextView tv7;
-    TextView tv8;
-
-    TextView tv1_1;
-    TextView tv2_1;
-    TextView tv3_1;
-    TextView tv4_1;
-    TextView tv5_1;
-    TextView tv6_1;
-    TextView tv7_1;
-    TextView tv8_1;
-
-    CheckBox[] cbList = {cb1, cb2, cb3, cb4, cb5, cb6, cb7, cb8};
-    TextView[] tvList = {tv1, tv2, tv3, tv4, tv5, tv6, tv7, tv8};
-    TextView[] tv_List = {tv1_1, tv2_1, tv3_1, tv4_1, tv5_1, tv6_1, tv7_1, tv8_1};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
 
-        //view들 initialize
-        this.InitializeView();
-
-        //button click event 처리
-        btn_cart.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SelectActivity.this, CartActivity.class);
-                intent.putExtra("checkedItem", GetCheckedItem()); //GetCheckedItem()의 리턴값인 checkedItem을 CartActivity로 넘김
-                intent.putExtra("checkedPrice", GetCheckedPrice()); //GetCheckedPrice()의 리턴값인 checkedPrice를 CartActivity로 넘김
-                startActivity(intent);
-            }
-        });
-
-        btn_buy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SelectActivity.this, BuyActivity.class);
-                intent.putExtra("checkedItem", GetCheckedItem()); //Checked()의 리턴값인 checkedItem을 CartActivity로 넘김
-                intent.putExtra("checkedPrice", GetCheckedPrice());
-                startActivity((intent));
-            }
-        });
-
-    }
-
-    public void InitializeView() { //view들을 initialize 해주는 함수
         Button btn_cart = (Button) findViewById(R.id.btn_cart);
         Button btn_buy = (Button) findViewById(R.id.btn_buy);
 
@@ -108,25 +48,51 @@ public class SelectActivity extends AppCompatActivity {
         TextView tv6_1 = (TextView) findViewById(R.id.tv6_1);
         TextView tv7_1 = (TextView) findViewById(R.id.tv7_1);
         TextView tv8_1 = (TextView) findViewById(R.id.tv8_1);
+
+        final CheckBox[] cbList = {cb1, cb2, cb3, cb4, cb5, cb6, cb7, cb8};
+        final TextView[] tvList = {tv1, tv2, tv3, tv4, tv5, tv6, tv7, tv8};
+        final TextView[] tv_List = {tv1_1, tv2_1, tv3_1, tv4_1, tv5_1, tv6_1, tv7_1, tv8_1};
+
+        //button click event 처리
+        btn_cart.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SelectActivity.this, CartActivity.class);
+                intent.putExtra("checkedItem", GetCheckedItem(cbList, tvList)); //GetCheckedItem()의 리턴값인 checkedItem을 CartActivity로 넘김
+                intent.putExtra("checkedPrice", GetCheckedPrice(cbList, tv_List)); //GetCheckedPrice()의 리턴값인 checkedPrice를 CartActivity로 넘김
+                startActivity(intent);
+            }
+        });
+
+        btn_buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SelectActivity.this, BuyActivity.class);
+                intent.putExtra("checkedItem", GetCheckedItem(cbList, tvList));
+                intent.putExtra("checkedPrice", GetCheckedPrice(cbList, tv_List));
+                startActivity((intent));
+            }
+        });
+
     }
 
-    public ArrayList<String> GetCheckedItem() {
+    public ArrayList<String> GetCheckedItem(CheckBox[] cbArr, TextView[] tvArr) { //check된 상품의 상품명을 ArrayList에 담아 리턴해주는 함수
         ArrayList<String> checkedItem = new ArrayList<String>();
 
         for(int i=0; i<8; i++) {
-            if(cbList[i].isChecked()) {
-                checkedItem.add(tvList[i].getText().toString());
+            if(cbArr[i].isChecked()) {
+                checkedItem.add(tvArr[i].getText().toString());
             }
         }
         return checkedItem;
     }
 
-    public ArrayList<String> GetCheckedPrice() {
+    public ArrayList<String> GetCheckedPrice(CheckBox[] cbArr, TextView[] tv_Arr) { //check된 상품의 가격을 ArrayList에 담아 리턴해주는 함수
         ArrayList<String> checkedPrice = new ArrayList<String>();
 
         for(int i=0; i<8; i++) {
-            if(cbList[i].isChecked()) {
-                checkedPrice.add(tv_List[i].getText().toString());
+            if(cbArr[i].isChecked()) {
+                checkedPrice.add(tv_Arr[i].getText().toString());
             }
         }
         return checkedPrice;
